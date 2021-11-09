84bdcb15197ffc9829fe92205bd72666fb69a5b4
Andres Garcia <andres.fgp@hotmail.com>
2021-11-08 20:18:34 -0500

    Feature Selectors

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,8 +1,13 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+export const selectAuthState=
+        createFeatureSelector<AuthState>("auth")
 
 export const isLoggedIn=createSelector( //Ngrx selector, it is a mapping function which has memory
-    state=>state["auth"],
-    (auth)=>!!auth.user
+    selectAuthState,
+    // state=>state["auth"],
+    auth => !!auth.user
 )
 export const isLoggedOut=createSelector( //Ngrx selector, it is a mapping function which has memory
     // state=>state["auth"],
