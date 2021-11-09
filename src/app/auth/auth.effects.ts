facf24507f68f5c81398da159eac37f3c0aae1fd
Andres Garcia <andres.fgp@hotmail.com>
2021-11-08 22:28:10 -0500

    Logout Effect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,18 +1,37 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { tap } from "rxjs/operators";
+import { AuthActions } from "./action-types";
 
 @Injectable()
 export class AuthEffects{
+
     constructor(
         private actions$:Actions
     ){
-        actions$.subscribe(action => {
-            if(action.type==="[Login Page] User Login"){
-                localStorage.setItem('user',JSON.stringify(action["user"])) //save in localStorage
-            }
-        });
+        // actions$.subscribe(action => {
+        //     if(action.type==="[Login Page] User Login"){
+        //         localStorage.setItem('user',JSON.stringify(action["user"])) //save in localStorage
+        //     }
+        // });
     }
 
-
+    login$=createEffect(()=>
+        this.actions$
+            .pipe(
+                ofType(AuthActions.login),
+                tap(action=> localStorage.setItem('user',JSON.stringify(action["user"]))) //save in localStorage)
+            ),
+        {dispatch:false}
+    )
+    
+    logout$=createEffect(()=>
+        this.actions$
+            .pipe(
+                ofType(AuthActions.logout),
+                tap(action=> localStorage.removeItem('user')) //save in localStorage)
+            ),
+        {dispatch:false}
+    )
     
 }
