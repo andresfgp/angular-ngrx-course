7eb7a0dac31c339078b153283cfe003a3d83d859
Andres Garcia <andres.fgp@hotmail.com>
2021-11-09 15:03:22 -0500

    Entity Data using a Router Resolver

---
+++ src/app/courses/courses.resolver.ts
@@ -0,0 +1,31 @@
+import { loadAllCourses } from './course.actions';
+import { AppState } from './../reducers/index';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { finalize, first, tap } from 'rxjs/operators';
+
+
+
+@Injectable()
+export class CoursesResolver implements Resolve<any> {
+
+    loading = false;
+
+    constructor(private store: Store<AppState>) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+        return this.store
+            .pipe(
+                tap(() => {
+                    if (!this.loading) {
+                        this.loading = true;
+                        this.store.dispatch(loadAllCourses());
+                    }
+                }),
+                first(),
+                finalize(() => this.loading = false)
+            );
+    }
+}
